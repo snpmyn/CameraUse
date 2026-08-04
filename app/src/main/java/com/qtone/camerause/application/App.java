7e43dc36d2b7f1820accdec76585826d6f395ff9
snpmyn <snpmyn@126.com>
2026-08-04 17:26:57 +0800

    App 添加百度 - OCR 初始化

--- app/src/main/java/com/qtone/camerause/application/App.java
+++ app/src/main/java/com/qtone/camerause/application/App.java
@@ -3,6 +3,10 @@ package com.qtone.camerause.application;
 import android.app.Application;
 import android.util.Log;
 
+import com.baidu.ocr.sdk.OCR;
+import com.baidu.ocr.sdk.OnResultListener;
+import com.baidu.ocr.sdk.exception.OCRError;
+import com.baidu.ocr.sdk.model.AccessToken;
 import com.jiangdg.ausbc.utils.ToastUtils;
 
 import org.opencv.android.OpenCVLoader;
@@ -21,10 +25,22 @@ public class App extends Application {
         ToastUtils.INSTANCE.init(this);
         // 初始化 OpenCV
         // 验证 OpenCV 依赖与 .so 库是否正常加载
-        if (OpenCVLoader.initLocal()) {
+        if (OpenCVLoader.initDebug()) {
             Log.d("OpenCV", "OpenCV 初始化成功");
         } else {
             Log.e("OpenCV", "OpenCV 初始化失败");
         }
+        // 初始化百度 - OCR
+        OCR.getInstance(this).initAccessToken(new OnResultListener<AccessToken>() {
+            @Override
+            public void onResult(AccessToken accessToken) {
+                Log.d("百度 - OCR", "百度 - OCR 初始化成功 || " + accessToken);
+            }
+
+            @Override
+            public void onError(OCRError ocrError) {
+                Log.d("百度 - OCR", "百度 - OCR 初始化错误 || " + ocrError.getMessage());
+            }
+        }, this);
     }
 }
