2279e73ba6a0ec04af9b0f53c6565be75de0193d
snpmyn <snpmyn@126.com>
2026-08-06 15:13:34 +0800

    扫码和拍照架构改造

---
+++ app/src/main/java/com/qtone/camerause/capture/CaptureMode.java
@@ -0,0 +1,18 @@
+package com.qtone.camerause.capture;
+
+/**
+ * Created on 2026/8/5.
+ *
+ * @author 郑少鹏
+ * @desc 拍照模式
+ */
+public enum CaptureMode {
+    /**
+     * 单拍
+     */
+    SINGLE,
+    /**
+     * 连拍
+     */
+    BURST
+}
