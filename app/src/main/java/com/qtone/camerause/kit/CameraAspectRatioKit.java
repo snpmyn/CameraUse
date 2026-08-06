a2d58497dbfa143fabc418f2ea0e6250e55fe16b
snpmyn <snpmyn@126.com>
2026-08-06 15:10:17 +0800

    扫码和拍照架构改造

--- app/src/main/java/com/qtone/camerause/kit/CameraAspectRatioKit.java
+++ app/src/main/java/com/qtone/camerause/kit/CameraAspectRatioKit.java
@@ -20,16 +20,16 @@ public class CameraAspectRatioKit {
      */
     private AspectRatioTextureView aspectRatioTextureView;
     /**
-     * 当前物理帧宽度
+     * 当前物理帧宽
      */
     private int currentWidth = -1;
     /**
-     * 当前物理帧高度
+     * 当前物理帧高
      */
     private int currentHeight = -1;
 
     /**
-     * 构造函数
+     * constructor
      *
      * @param aspectRatioTextureView AspectRatioTextureView
      */
@@ -39,26 +39,22 @@ public class CameraAspectRatioKit {
 
     /**
      * 更新宽高比
-     * <p>
-     * 根据相机抛出的实际帧宽高动态更新 AspectRatioTextureView 显示比例
      *
-     * @param activity 用于切回主线程更新 UI
-     * @param width    物理帧宽度
-     * @param height   物理帧高度
+     * @param activity 活动
+     * @param width    物理帧宽
+     * @param height   物理帧高
      */
     public void updateAspectRatio(@Nullable Activity activity, int width, int height) {
         if ((width <= 0) || (height <= 0) || (aspectRatioTextureView == null) || (activity == null)) {
             return;
         }
-        // 状态检查
-        // 只有当分辨率确实发生变化时才更新布局，避免高频触发 requestLayout() 造成卡顿。
+        // 仅当分辨率确实发生变化时才更新布局，避免高频触发 requestLayout() 导致卡顿。
         if ((currentWidth != width) || (currentHeight != height)) {
             currentWidth = width;
             currentHeight = height;
             float ratio = (float) width / (float) height;
-            Log.d(TAG, String.format("动态更新预览比例: %d:%d (宽高比: %.2f)", width, height, ratio));
+            Log.d(TAG, String.format("更新预览比例 || %d:%d (宽高比 %.2f)", width, height, ratio));
             activity.runOnUiThread(() -> {
-                // 确保 Activity 和 View 依然处于正常生命周期内
                 if (!activity.isFinishing() && !activity.isDestroyed() && (aspectRatioTextureView != null)) {
                     aspectRatioTextureView.setAspectRatio(width, height);
                 }
@@ -69,8 +65,10 @@ public class CameraAspectRatioKit {
     /**
      * 重置
      * <p>
+     * 相机重启时调
+     * 切换设备时调
+     * <p>
      * 重置缓存的分辨率记录
-     * 如在相机重启、切换设备时调用
      */
     public void reset() {
         this.currentWidth = -1;
@@ -79,9 +77,6 @@ public class CameraAspectRatioKit {
 
     /**
      * 释放
-     * <p>
-     * 释放 View 引用
-     * 防止 Fragment 销毁后子线程异步回调导致的内存泄漏
      */
     public void release() {
         this.aspectRatioTextureView = null;
