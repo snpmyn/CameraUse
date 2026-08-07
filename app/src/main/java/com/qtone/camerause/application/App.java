521b831dc7c8f6bdb4a7c147f6d71ef027b0bc59
snpmyn <snpmyn@126.com>
2026-08-07 13:37:47 +0800

    扫码和拍照架构改造

--- app/src/main/java/com/qtone/camerause/application/App.java
+++ app/src/main/java/com/qtone/camerause/application/App.java
@@ -8,6 +8,7 @@ import com.baidu.ocr.sdk.OnResultListener;
 import com.baidu.ocr.sdk.exception.OCRError;
 import com.baidu.ocr.sdk.model.AccessToken;
 import com.jiangdg.ausbc.utils.ToastUtils;
+import com.qtone.camerause.kit.LogKit;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -26,20 +27,20 @@ public class App extends Application {
         // 初始化 OpenCV
         // 验证 OpenCV 依赖与 .so 库是否正常加载
         if (OpenCVLoader.initDebug()) {
-            Log.d("OpenCV", "OpenCV 初始化成功");
+            Log.d(LogKit.TAG, "OpenCV 初始化成功");
         } else {
-            Log.e("OpenCV", "OpenCV 初始化失败");
+            Log.e(LogKit.TAG, "OpenCV 初始化失败");
         }
-        // 初始化百度 - OCR
+        // 初始化百度 OCR
         OCR.getInstance(this).initAccessToken(new OnResultListener<AccessToken>() {
             @Override
             public void onResult(AccessToken accessToken) {
-                Log.d("百度 - OCR", "百度 - OCR 初始化成功 || " + accessToken);
+                Log.d(LogKit.TAG, "百度 OCR 初始化成功 || " + accessToken);
             }
 
             @Override
             public void onError(OCRError ocrError) {
-                Log.d("百度 - OCR", "百度 - OCR 初始化错误 || " + ocrError.getMessage());
+                Log.d(LogKit.TAG, "百度 OCR 初始化错误 || " + ocrError.getMessage());
             }
         }, this);
     }
