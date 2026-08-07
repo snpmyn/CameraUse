79c1f71f6be92caca098a374ea175a820f3ba4cc
snpmyn <snpmyn@126.com>
2026-08-07 13:48:26 +0800

    扫码和拍照架构改造

--- app/src/main/java/com/qtone/camerause/kit/AssetFileKit.java
+++ app/src/main/java/com/qtone/camerause/kit/AssetFileKit.java
@@ -17,32 +17,29 @@ import java.io.InputStream;
  * @desc Asset 文件配套原件
  */
 public class AssetFileKit {
-    private static final String TAG = AssetFileKit.class.getSimpleName();
-
     /**
      * 拷贝 Asset 文件到缓存
      *
      * @param context          上下文
      * @param subdirectoryName 子目录名
-     *                         若在根目录传 "" 或 null
+     *                         需根目录传 null 或 ""
      * @param fileName         文件名
-     * @return 物理绝对路径 (拷贝失败或目录创建失败时返回空字符串 "")
+     * @return 物理绝对路径 (拷贝失败或目录创建失败返 "")
      */
     @NotNull
     public static String copyAssetFileToCache(@NotNull Context context, String subdirectoryName, String fileName) {
         if (TextUtils.isEmpty(fileName)) {
-            Log.e(TAG, "文件名不能为空");
+            Log.e(LogKit.TAG, "文件名不能为空");
             return "";
         }
-        // 1. 处理目录路径
-        // dirName 为 null 或 "" 时，直接使用 getFilesDir() 根目录。
+        // subdirectoryName 为 null 或 "" 时直接使用 getFilesDir() 根目录
         boolean hasDir = (subdirectoryName != null) && !subdirectoryName.trim().isEmpty();
         File dir = hasDir ? new File(context.getFilesDir(), subdirectoryName.trim()) : context.getFilesDir();
         // 判断目录创建结果
         if (!dir.exists()) {
             boolean isCreated = dir.mkdirs();
             if (!isCreated && !dir.exists()) {
-                Log.e(TAG, "创建目标文件夹失败 || " + dir.getAbsolutePath());
+                Log.e(LogKit.TAG, "创建目标文件夹失败 || " + dir.getAbsolutePath());
                 return "";
             }
         }
@@ -51,8 +48,7 @@ public class AssetFileKit {
         if (file.exists() && (file.length() > 0)) {
             return file.getAbsolutePath();
         }
-        // 2. 拼接 Asset 中的相对路径
-        // dirName 为 null 或 "" 时，直接读取 assets 根目录。
+        // subdirectoryName 为 null 或 "" 时直接读取 assets 根目录
         String assetPath = hasDir ? (subdirectoryName.trim() + "/" + fileName) : fileName;
         try (InputStream inputStream = context.getAssets().open(assetPath);
              FileOutputStream fileOutputStream = new FileOutputStream(file)) {
@@ -62,14 +58,14 @@ public class AssetFileKit {
                 fileOutputStream.write(buffer, 0, byteCount);
             }
             fileOutputStream.flush();
-            Log.d(TAG, "成功复制模型文件 || " + assetPath);
+            Log.d(LogKit.TAG, "复制文件成功 || " + assetPath);
         } catch (Exception e) {
-            Log.e(TAG, "复制模型文件异常 || " + assetPath, e);
-            // 判断残缺文件删除结果
+            Log.e(LogKit.TAG, "复制文件异常 || " + assetPath, e);
+            // 判断未完整写入的残缺文件删除结果
             if (file.exists()) {
                 boolean isDeleted = file.delete();
                 if (!isDeleted) {
-                    Log.w(TAG, "删除未完整写入的残缺文件失败 || " + file.getAbsolutePath());
+                    Log.w(LogKit.TAG, "删除未完整写入的残缺文件失败 || " + file.getAbsolutePath());
                 }
             }
             return "";
