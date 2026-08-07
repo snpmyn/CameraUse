fabea72948c72da5924936c2309d23e42d0d632a
snpmyn <snpmyn@126.com>
2026-08-07 13:49:20 +0800

    扫码和拍照架构改造

---
+++ app/src/main/java/com/qtone/camerause/kit/LogKit.java
@@ -0,0 +1,18 @@
+package com.qtone.camerause.kit;
+
+/**
+ * Created on 2026/8/7.
+ *
+ * @author 郑少鹏
+ * @desc Log 配套原件
+ */
+public class LogKit {
+    public static final String TAG = "CU";
+
+    /**
+     * constructor
+     */
+    private LogKit() {
+
+    }
+}
