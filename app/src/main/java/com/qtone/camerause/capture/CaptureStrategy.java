60ac73d15267161e59bf58c73a0cb9ab415c6de3
snpmyn <snpmyn@126.com>
2026-08-09 19:12:39 +0800

    提交

---
+++ app/src/main/java/com/qtone/camerause/capture/CaptureStrategy.java
@@ -0,0 +1,18 @@
+package com.qtone.camerause.capture;
+
+/**
+ * Created on 2026/8/8.
+ *
+ * @author 郑少鹏
+ * @desc 拍照策略
+ */
+public enum CaptureStrategy {
+    /**
+     * NV21 帧
+     */
+    NV21_FRAME,
+    /**
+     * SDK 拍照
+     */
+    SDK_CAPTURE
+}
