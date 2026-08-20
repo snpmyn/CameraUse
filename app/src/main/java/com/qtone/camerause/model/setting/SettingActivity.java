ea8cb51cf52834b0b0bdeaf6a98b2525f7caeaa2
snpmyn <snpmyn@126.com>
2026-08-20 17:22:19 +0800

    提交

---
+++ app/src/main/java/com/qtone/camerause/model/setting/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.qtone.camerause.model.setting;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.qtone.camerause.R;
+import com.qtone.camerause.model.setting.fragment.SettingFragment;
+import com.qtone.camerause.widget.transition.kit.TransitionKit;
+
+/**
+ * @decs: 设置页
+ * @author: 郑少鹏
+ * @date: 2026/8/19 17:42
+ * @version: v 1.0
+ */
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TransitionKit.getInstance().endPageSetting(this, 500L);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        getSupportFragmentManager().beginTransaction().replace(R.id.settingActivityFl, new SettingFragment()).commit();
+    }
+}
