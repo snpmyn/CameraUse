521613cd9e34e79b5298e6eaf6a278425d9bf393
snpmyn <snpmyn@126.com>
2026-08-21 15:33:45 +0800

    提交

---
+++ app/src/main/java/com/qtone/camerause/value/IntentConstant.java
@@ -0,0 +1,14 @@
+package com.qtone.camerause.value;
+
+/**
+ * Created on 2026/8/21.
+ *
+ * @author 郑少鹏
+ * @desc 意图常量
+ */
+public class IntentConstant {
+    /**
+     * 图库页 (文件夹条目传输)
+     */
+    public static final String GALLERY_ACTIVITY_$_FOLDER_ITEM = "GalleryActivityFolderItem";
+}
