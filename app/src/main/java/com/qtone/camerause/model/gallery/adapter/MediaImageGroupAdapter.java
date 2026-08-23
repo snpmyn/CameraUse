5bb87ed7b61366e573ac1865ce58f1ac2d6c3471
snpmyn <snpmyn@126.com>
2026-08-23 15:02:41 +0800

    提交

--- app/src/main/java/com/qtone/camerause/model/gallery/adapter/MediaImageGroupAdapter.java
+++ app/src/main/java/com/qtone/camerause/model/gallery/adapter/MediaImageGroupAdapter.java
@@ -11,10 +11,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.qtone.camerause.R;
-import com.qtone.camerause.function.media.MediaScanner;
 import com.qtone.camerause.util.density.DensityUtils;
 import com.qtone.camerause.util.image.GlideLoader;
 import com.qtone.camerause.util.list.ListUtils;
+import com.qtone.camerause.util.media.MediaScanner;
 import com.qtone.camerause.util.screen.ScreenUtils;
 import com.qtone.camerause.widget.recyclerview.listener.OnRecyclerViewOnItemClickListener;
 
