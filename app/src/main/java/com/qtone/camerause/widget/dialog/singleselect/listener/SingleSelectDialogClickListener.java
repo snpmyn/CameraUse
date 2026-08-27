d71d50623daf11bcd603bb3d3b184e2a57d83a54
snpmyn <snpmyn@126.com>
2026-08-27 14:07:06 +0800

    提交

--- app/src/main/java/com/qtone/camerause/widget/dialog/singleselect/listener/SingleSelectDialogClickListener.java
+++ app/src/main/java/com/qtone/camerause/widget/dialog/singleselect/listener/SingleSelectDialogClickListener.java
@@ -1,29 +1,20 @@
-package com.qtone.camerause.widget.dialog.listener;
+package com.qtone.camerause.widget.dialog.singleselect.listener;
 
 import com.qtone.camerause.widget.dialog.base.BaseLifecycleDialog;
 
 /**
- * Created on 2026/8/26.
+ * Created on 2026/8/27.
  *
  * @author 郑少鹏
- * @desc 对话框点击监听
+ * @desc 单选对话框点击监听
  */
-public interface DialogClickListener {
+public interface SingleSelectDialogClickListener {
     /**
      * 取消
      *
      * @param baseLifecycleDialog 生命周期对话框基类
      */
     default void onCancel(BaseLifecycleDialog baseLifecycleDialog) {
-
-    }
-
-    /**
-     * 确认
-     *
-     * @param baseLifecycleDialog 生命周期对话框基类
-     */
-    default void onConfirm(BaseLifecycleDialog baseLifecycleDialog) {
 
     }
 
