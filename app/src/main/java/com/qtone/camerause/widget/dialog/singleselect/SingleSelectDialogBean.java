a11368b09cc7f35ac4015df0a37233c2ea96b00b
snpmyn <snpmyn@126.com>
2026-08-27 09:39:30 +0800

    提交

---
+++ app/src/main/java/com/qtone/camerause/widget/dialog/singleselect/SingleSelectDialogBean.java
@@ -0,0 +1,45 @@
+package com.qtone.camerause.widget.dialog.singleselect;
+
+/**
+ * Created on 2026/8/26.
+ *
+ * @author 郑少鹏
+ * @desc 单选对话框数据
+ */
+public class SingleSelectDialogBean {
+    /**
+     * 内容
+     */
+    private String content;
+    /**
+     * 是否选中
+     */
+    private boolean isChecked;
+
+    /**
+     * constructor
+     *
+     * @param content   内容
+     * @param isChecked 是否选中
+     */
+    public SingleSelectDialogBean(String content, boolean isChecked) {
+        this.content = content;
+        this.isChecked = isChecked;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+}
