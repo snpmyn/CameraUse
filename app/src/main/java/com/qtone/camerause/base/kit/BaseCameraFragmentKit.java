b60228bf310b3c2aa09338f333a92f0cfe3a2ef5
zsp <zhengshaopeng@qtone.cn>
2026-09-02 16:39:44 +0800

    架构调整 - 相机碎片基类配套原件删除相机设置逻辑

--- app/src/main/java/com/qtone/camerause/base/kit/BaseCameraFragmentKit.java
+++ app/src/main/java/com/qtone/camerause/base/kit/BaseCameraFragmentKit.java
@@ -1,9 +1,6 @@
 package com.qtone.camerause.base.kit;
 
-import android.util.Log;
-
 import com.qtone.camerause.base.BaseCameraFragment;
-import com.qtone.camerause.util.log.LogKit;
 import com.qtone.camerause.widget.camera.CameraController;
 
 /**
@@ -32,39 +29,5 @@ public class BaseCameraFragmentKit {
      */
     public void setAutoFocus() {
         CameraController.getInstance().setAutoFocus(baseCameraFragment.getCurrentCamera(), true);
-    }
-
-    /**
-     * 相机设置
-     */
-    public void cameraSetting() {
-        // 亮度
-        int brightness = 43;
-        CameraController.getInstance().setBrightness(baseCameraFragment.getCurrentCamera(), brightness);
-        Log.d(LogKit.TAG, "亮度 - 最优 || " + brightness);
-        // 对比度
-        int contrast = 58;
-        CameraController.getInstance().setContrast(baseCameraFragment.getCurrentCamera(), contrast);
-        Log.d(LogKit.TAG, "对比度 - 最优 || " + contrast);
-        // 增益
-        int gain = 6;
-        CameraController.getInstance().setGain(baseCameraFragment.getCurrentCamera(), gain);
-        Log.d(LogKit.TAG, "增益 - 最优 || " + gain);
-        // Gamma
-        int gamma = 31;
-        CameraController.getInstance().setGamma(baseCameraFragment.getCurrentCamera(), gamma);
-        Log.d(LogKit.TAG, "Gamma - 最优 || " + gamma);
-        // 色调
-        int hue = 52;
-        CameraController.getInstance().setHue(baseCameraFragment.getCurrentCamera(), hue);
-        Log.d(LogKit.TAG, "色调 - 最优 || " + hue);
-        // 锐度
-        int sharpness = 60;
-        CameraController.getInstance().setSharpness(baseCameraFragment.getCurrentCamera(), sharpness);
-        Log.d(LogKit.TAG, "锐度 - 最优 || " + sharpness);
-        // 饱和度
-        int saturation = 82;
-        CameraController.getInstance().setSaturation(baseCameraFragment.getCurrentCamera(), saturation);
-        Log.d(LogKit.TAG, "饱和度 - 最优 || " + saturation);
     }
 }
