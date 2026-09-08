0dfd8f832a273189b1987bc3ea06430efaf8569b
zsp <zhengshaopeng@qtone.cn>
2026-09-08 14:04:17 +0800

    相机参数设置新增恢复设置

--- app/src/main/java/com/qtone/camerause/base/kit/BaseCameraFragmentKit.java
+++ app/src/main/java/com/qtone/camerause/base/kit/BaseCameraFragmentKit.java
@@ -37,5 +37,6 @@ public class BaseCameraFragmentKit {
      */
     public void cameraSetting() {
         CameraSettingKit.cameraSetting(baseCameraFragment.getCurrentCamera());
+        /*CameraSettingKit.restoreBrightness(baseCameraFragment.getCurrentCamera());*/
     }
 }
