036cb06847fac9b71f95b8d903eab1c645637c4f
zsp <zhengshaopeng@qtone.cn>
2026-09-08 14:12:33 +0800

    相机设置对话框 - 重写 startLogic

--- app/src/main/java/com/qtone/camerause/widget/dialog/camerasetting/CameraSettingDialog.java
+++ app/src/main/java/com/qtone/camerause/widget/dialog/camerasetting/CameraSettingDialog.java
@@ -213,6 +213,14 @@ public class CameraSettingDialog extends BaseLifecycleDialog {
     }
 
     /**
+     * 开始逻辑
+     */
+    @Override
+    protected void startLogic() {
+
+    }
+
+    /**
      * 清理资源
      * <p>
      * 对话框从 Window 移除时触发
