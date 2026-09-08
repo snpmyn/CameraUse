fe93573987e682ed06a00b9e7583d548a0473a27
zsp <zhengshaopeng@qtone.cn>
2026-09-08 14:13:04 +0800

    单选对话框 - 重写 startLogic

--- app/src/main/java/com/qtone/camerause/widget/dialog/singleselect/SingleSelectDialog.java
+++ app/src/main/java/com/qtone/camerause/widget/dialog/singleselect/SingleSelectDialog.java
@@ -154,6 +154,14 @@ public class SingleSelectDialog extends BaseLifecycleDialog {
     }
 
     /**
+     * 开始逻辑
+     */
+    @Override
+    protected void startLogic() {
+
+    }
+
+    /**
      * 清理资源
      * <p>
      * 对话框从 Window 移除时触发
